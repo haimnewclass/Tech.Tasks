c247f924b08e0a789a89ae28ff1e942c22ffd97b
haimnewclass <56435506+haimnewclass@users.noreply.github.com>
2022-04-10 11:10:07 +0300

    Add project files.

---
+++ Tech.Tasks/Pen.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tech.Tasks
+{
+    public class Pen
+    {
+        public enum Food {Banana, Apple, Bread, Chocolad};
+
+        public delegate string DrawCrown();
+        public string Draw(string str, DrawCrown func)
+        {
+            return str + "*"+func()+"*" + str;
+        }
+
+    }
+}
---
+++ Tech.Tasks/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tech.Tasks
+{
+    internal class Program
+    {
+        void func()
+        {
+
+        }
+        static void Main(string[] args)
+        {
+            MyPaint myPaint = new MyPaint();
+            myPaint.Run();            
+        }
+
+    }
+
+
+    public class MyPaint
+    {
+        Pen.Food food;
+        public string BritishCrown(int diamonds)
+        {
+            return "<**"+diamonds.ToString()+"*>";
+        }
+        public string AmericanCrown()
+        {
+            return "<$$$>";
+        }
+        public string EuropeanCrown()
+        {
+            return "<^^^>";
+        }
+        public string EmptyCrown()
+        {
+            return "<   >";
+        }
+        public void Run()
+        {
+
+            Pen.DrawCrown a;
+            Pen.DrawCrown b;
+
+            a = EuropeanCrown;
+
+            Console.WriteLine(a());
+
+            a = AmericanCrown;
+
+            Console.WriteLine(a());
+
+            Pen p = new Pen();
+            Console.WriteLine(p.Draw("Aba", AmericanCrown));
+            Console.WriteLine(p.Draw("Aba", EuropeanCrown));
+        }
+    }
+
+}
